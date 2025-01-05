3a57eb2b258f0e59289b2bf0c54cafeda3b301f1
dtcpay-xiangli <xiang.li@dtcpay.com>
2025-01-05 18:00:11 +0800

    init project

---
+++ src/main/java/com/example/demo/DemoSpringBootSwaggerv3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootSwaggerv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootSwaggerv3Application.class, args);
+    }
+
+}
